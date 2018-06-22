d28963830c6be77d02b5859ff749381ef0938e9e
dk\ivd <ivd@sitecore.net>
2018-06-22 15:07:08 +0800

    225587: Resolved issue

---
+++ src/Sitecore.Support.225587/CloudFieldMap.cs
@@ -0,0 +1,40 @@
+﻿
+namespace Sitecore.Support.ContentSearch.Azure.FieldMaps
+{
+  using System;
+  using System.Collections.Generic;
+  using System.Xml;
+  using Diagnostics;
+  using Sitecore.ContentSearch.Azure;
+  using Sitecore.ContentSearch.Azure.FieldMaps;
+
+  public class CloudFieldMap : Sitecore.ContentSearch.Azure.FieldMaps.CloudFieldMap, ICloudFieldMap
+  {
+    CloudSearchFieldConfiguration ICloudFieldMap.GetCloudFieldConfigurationByFieldName(string fieldName)
+    {
+      var configuration = this.GetFieldConfiguration(fieldName);
+      return configuration as CloudSearchFieldConfiguration;
+    }
+
+    public override void AddFieldByFieldName(string fieldName, Type settingType, IDictionary<string, string> attributes, XmlNode configNode)
+    {
+      base.AddFieldByFieldName(fieldName, settingType, attributes, configNode);
+
+      string cloudFieldName;
+      if (!attributes.TryGetValue(CloudSearchFieldConfiguration.CloudFieldNameName, out cloudFieldName))
+      {
+        cloudFieldName = fieldName;
+      }
+
+      var configuration = this.fieldNameMap[fieldName.ToLowerInvariant()] as CloudSearchFieldConfiguration;
+      if (configuration != null)
+      {
+        this.cloudFieldNameMap[cloudFieldName] = configuration;
+      }
+      else
+      {
+        Log.Info(string.Format(Texts.WrongFieldConfigurationType, fieldName), this);
+      }
+    }
+  }
+}
---
+++ src/Sitecore.Support.225587/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.225587")]
+[assembly: AssemblyProduct("Sitecore.Support.225587")]
+[assembly: ComVisible(false)]
